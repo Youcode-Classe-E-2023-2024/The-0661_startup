df4aaac2cf556a0f9b8bc0e45b720f692a02ddc2
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2023-11-03 21:46:34 +0100

    fixing all ali's local.storage by the scrum master

--- script/localStorage.js
+++ script/localStorage.js
@@ -1,34 +1,65 @@
 var eee = document.querySelectorAll(".addToShop");
-let names = document.querySelectorAll(".nameP")
-let imgHide = document.getElementById("imgHide")
-let items = document.getElementById("heading")
-let price = document.querySelectorAll(".price")
-var priceSpan = document.getElementById("priceSpan")
-var total = 0;
-var empty = document.getElementById("empty")
+var names = document.querySelectorAll(".nameP");
+var imgHide = document.getElementById("imgHide");
+var items = document.getElementById("items");
+var totalSpan = document.getElementById("totalSpan")
+var price = document.querySelectorAll(".price");
+var imageProducts = document.querySelectorAll(".imgP");
 
-var key = 0;
+var priceSpan = document.getElementById("priceSpan");
+var total = localStorage.getItem('total') ? parseFloat(localStorage.getItem('total')) : 0;
+
+var empty = document.getElementById("empty");
+
+var key = localStorage.length;
 
 for (let i = 0; i < eee.length; i++) {
-    eee[i].addEventListener("click", function() {
-        let nu = price[i].textContent;
-        total += parseFloat(nu);
-        let concat = {
-            name: names[i].textContent, 
-            price: price[i].textContent,
+    eee[i].addEventListener("click", function () {
+       // window.location.href = "http://127.0.0.1:5500/panier.html"
+        var nu = parseFloat(price[i].textContent);
+     
+        total += nu;
+     
+      
+        var concat = {
+            name: names[i].textContent,
+            imageProduct: imageProducts[i].src,
+            price: nu,
             total: total
-        }
-        localStorage.setItem(key++, JSON.stringify(concat)); 
+        };
+       
+        localStorage.setItem(key++, JSON.stringify(concat));
+        localStorage.setItem("total", total); 
+        
     });
 }
 
-if(localStorage.length > 0){
+if (localStorage.length > 0) {
     imgHide.style.display = "none";
-    empty.style.display = "none"
-    for(let x = 0; x < localStorage.length; x++){
-        var item = JSON.parse(localStorage.getItem(x)); 
-        items.innerHTML += item.name;
-        priceSpan.innerHTML += item.price + "MAD"
+    items.style.display = "block"
+
+    for (let x = 0; x < localStorage.length; x++) {
+        var item = JSON.parse(localStorage.getItem(x));
+        
+        items.insertAdjacentHTML('beforeend', ` <div class="urmama  bg-gray-100 flex justify-around items-center p-2 w-full h-[29%]">
+        <div class="prdctcntiner h-[100px] bg-red-500 w-[75px]">
+            <img src="${item.imageProduct}" class="w-full h-full" alt="">
+        </div>
+        <div class="comer font-bold text-md pt-4">
+            <p>${item.name}</p>
+        </div>
+        <h1 class="font-bold">${item.price} MAD</h1>
+    
+    
+    </div>`);
+
+    // console.log(parseFloat(localStorage.getItem("total")) )
+    totalSpan.innerText = parseFloat(localStorage.getItem("total")) + " MAD";
+
     }
+    
+   
+}else{
+    items.style.display = "none"
+    document.getElementById("containerPanier").style.display= "none"
 }
-items.innerHTML +="<br>" + "Total: " + item.total;
